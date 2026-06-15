e23a476ffcf46de77629735590aaf3eb7240febe
Fábio Júnio Oliveira da Silva Filho <fjosf1@aluno.ifal.edu.br>
2026-06-15 11:05:46 -0300

    parameter router server.js

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const app = express();
 // TODO: GET /usuarios/:id → 200, JSON { "id": "<valor do parâmetro>" }
 // O Content-Type deve ser application/json
 
+app.get("/usuarios/:id", (req, res)=>{
+    res.type("application/json")
+    res.send({"id": req.params.id})
+})
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
